7feabf74348df0e7c6385848e5f5ea574136c6bc
Ahmad <hereahmad@gmail.com>
2023-11-01 01:19:29 +0300

    change main entry

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-// bottom sheet
-// export { default } from './components/bottomSheet';
 //
 export { bottom } from './contexts/useBottomStore';
 export { useBottomStore } from './contexts/useBottomStore';
 export { BottomsProvider as Provider }  from './components/provider'
 export { Bottom, Sheet } from './components/react';
-// bottom sheet modal
-// export { default as BottomSheetModal } from './components/bottomSheetModal';
-// export { default as BottomSheetModalProvider } from './components/bottomSheetModalProvider';
-
-//#region hooks
-// export { useBottomSheet } from './hooks/useBottomSheet';
-
-//#endregion
+//#end
